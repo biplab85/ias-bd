246272663e9aa15b772a8aa08f872a299c5d57a3
Biplab Paul <biplab@codersbucket.com>
2022-10-23 18:44:24 +0600

    New design

---
+++ pages/career-counselling.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap 5 Quick Template</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" type="text/css" href="../assets/css/slick.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/slick-theme.css" />
+</head>
+
+<body>
+
+
+
+    <!-- Top Header container S-->
+    <?php include '../pages/top-header.php'; ?>
+    <!-- Top Header container E-->
+
+    <!-- Menu Container S-->
+    <?php include '../pages/menu.php'; ?>
+    <!-- Menu Container E-->
+
+    <!-- Career Counselling S -->
+    <section class="career-counselling-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="career-counselling-container">
+                        <div class="career-counselling-left-asside">
+                            <div class="career-counselling-img">
+                                <img class="banner-bg" src="../assets/image/carrer-conselling.svg" alt="" title="">
+                            </div>
+                            <div class="career-counselling-desc">
+                                <h3>Career Counselling</h3>
+                                <p>
+                                    Now a day it is very hard to search professional student consultancy firm. We come for the career advice in and career counseling firm. It is one of the best education, consulting firm in Bangladesh.
+                                </p>
+                                <p>
+                                    We will review your academic, professional, financial circumstances along with your future study & the career plan. According to your needs, we will assist you in selecting the suitable course, the place of study & all other related issues. You will get the proper guidance from us which will fully comply with update visa requirements.
+                                </p>
+                            </div>
+
+                            <div class="career-counselling-social-share">
+                                <div class="share-title">
+                                    <img class="auth" src="../assets/image/blog/share.svg" alt="" title="">
+                                    <span>Share on</span>
+                                </div>
+                                <div class="social-link">
+                                    <a href="#">
+                                        <img class="banner-bg" src="../assets/image/icon/social-icon/color/facebook.svg" alt="" title="">
+                                    </a>
+                                    <a href="#">
+                                        <img class="banner-bg" src="../assets/image/icon/social-icon/color/twiter.svg" alt="" title="">
+                                    </a>
+                                    <a href="#">
+                                        <img class="banner-bg" src="../assets/image/icon/social-icon/color/linkdin.svg" alt="" title="">
+                                    </a>
+                                    <a href="#">
+                                        <img class="banner-bg" src="../assets/image/icon/social-icon/color/pinteret.svg" alt="" title="">
+                                    </a>
+                                </div>
+                            </div>
+
+                            <section class="consultations-from">
+                                <div class="consultations-from-container">
+                                    <div class="img-container">
+                                        <img class="banner-bg" src="../assets/image/consultation-from/cunsultation-from.svg" alt="" title="">
+                                    </div>
+                                    <div class="content">
+                                        <h2>Want to get the right direction? Make an appointment</h2>
+                                        <p>
+                                            Just book an appointment and get the advise from our experts to transform your dream into reality.
+                                        </p>
+                                        <div class="btn-container">
+                                            <a class="ias-btn outline shadow-none" href="#">Book Appointment Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                        <div class="career-counselling-right-asside">
+                            <div class="service">
+                                <h3 class="title">More Services</h3>
+                                <div>
+                                    <div class="card">
+                                        <div class="icon">
+                                            <img src="../assets/image/icon/service/icon-counselling.svg" alt="">
+                                        </div>
+                                        <h4 class="card-title">Career Counselling</h4>
+                                        <p>
+                                            We will review your academic, professional, financial circumstances along with...
+                                        </p>
+                                        <div class="btn-container text-center">
+                                            <a class="w-100 ias-btn outline shadow-none" href="#">Read More</a>
+                                        </div>
+                                    </div>
+                                    <div class="card">
+                                        <div class="icon">
+                                            <img src="../assets/image/icon/service/icon-counselling.svg" alt="">
+                                        </div>
+                                        <h4 class="card-title">Career Counselling</h4>
+                                        <p>
+                                            We will review your academic, professional, financial circumstances along with...
+                                        </p>
+                                        <div class="btn-container text-center">
+                                            <a class="w-100 ias-btn outline shadow-none" href="#">Read More</a>
+                                        </div>
+                                    </div>
+                                    <div class="card">
+                                        <div class="icon">
+                                            <img src="../assets/image/icon/service/icon-counselling.svg" alt="">
+                                        </div>
+                                        <h4 class="card-title">Career Counselling</h4>
+                                        <p>
+                                            We will review your academic, professional, financial circumstances along with...
+                                        </p>
+                                        <div class="btn-container text-center">
+                                            <a class="w-100 ias-btn outline shadow-none" href="#">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="service featured-services">
+                                <h3 class="title">Featured Services</h3>
+                                <div>
+                                    <div class="card">
+                                        <div class="icon">
+                                            <img src="../assets/image/icon/service/icon-counselling.svg" alt="">
+                                        </div>
+                                        <h4>Career Counselling</h4>
+                                        <p>
+                                            We will review your academic, professional, financial circumstances along with...
+                                        </p>
+                                        <div class="btn-container text-center">
+                                            <a class="w-100 ias-btn outline shadow-none" href="#">Read More</a>
+                                        </div>
+                                    </div>
+                                    <div class="card">
+                                        <div class="icon">
+                                            <img src="../assets/image/icon/service/icon-counselling.svg" alt="">
+                                        </div>
+                                        <h4>Career Counselling</h4>
+                                        <p>
+                                            We will review your academic, professional, financial circumstances along with...
+                                        </p>
+                                        <div class="btn-container text-center">
+                                            <a class="w-100 ias-btn outline shadow-none" href="#">Read More</a>
+                                        </div>
+                                    </div>
+                                    <div class="card">
+                                        <div class="icon">
+                                            <img src="../assets/image/icon/service/icon-counselling.svg" alt="">
+                                        </div>
+                                        <h4>Career Counselling</h4>
+                                        <p>
+                                            We will review your academic, professional, financial circumstances along with...
+                                        </p>
+                                        <div class="btn-container text-center">
+                                            <a class="w-100 ias-btn outline shadow-none" href="#">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Career Counselling E -->
+
+
+
+
+    <!-- Footer S-->
+    <?php include '../pages/footer.php'; ?>
+    <!-- Footer E-->
+
+
+
+
+    <script src="../assets/js/bundle.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="../assets/js/slick.js"></script>
+    <script type="text/javascript" src="../assets/js/menu.js"></script>
+    <script type="text/javascript" src="../assets/js/custom.js"></script>
+</body>
+
+</html>
