0bf95093e6963c057f4e8d0b0e84e959fee45188
Biplab Paul <biplab@codersbucket.com>
2022-10-25 20:44:04 +0600

    css update

--- pages/destination-success-stories.php
+++ pages/destination-success-stories.php
@@ -40,56 +40,7 @@
     <!-- Content Header E-->
 
     <!-- Filter S -->
-    <div class="filter-menu-wrapper">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light bg-transparent">
-            <div class="container">
-                <div class="filter-text d-inline-block d-lg-none">Filter</div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarFilter" aria-controls="navbarFilter" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarFilter">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">About Australia</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Study in Australia</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Scholarship in Australia</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Working Rights</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Admission Requirements</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Document Checklist</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Universities & Colleges</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Cost of Study</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Application Steps</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Success Stories</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Blogs</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">FAQs</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
+    <?php include '../pages/destination-filter.php'; ?>
     <!-- Filter E -->
 
     <!-- Destination Body Content Area S -->
