34e51a18083203a7fac2a63b652e4e13b4eb6b5d
Biplab Paul <biplab@codersbucket.com>
2022-12-25 14:12:04 +0600

    css update

---
+++ pages/university-details.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IASBD</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="http://design.iasbd.co.uk/assets/css/slick.css" />
+    <link rel="stylesheet" type="text/css" href="http://design.iasbd.co.uk/assets/css/slick-theme.css" />
+</head>
+
+<body>
+
+
+
+    <!-- Top Header container S-->
+    <?php include '../pages/top-header.php'; ?>
+    <!-- Top Header container E-->
+
+    <!-- Menu Container S-->
+    <?php include '../pages/menu.php'; ?>
+    <!-- Menu Container E-->
+
+    <section class="ias-header top-bg h-345 mb-0">
+        <div class="container">
+            <div class="col-md-12">
+                <div class="content-wrapper text-center">
+                    <div class="content-header-section white text-center">
+                        <h3 class="title with-bg d-inline-block mx-auto">University of Sydney</h3>
+                    </div>
+
+                    <div class="description with-transparent-bg text-center mx-auto d-inline-block px-15">Lorem Ipsum is simply dummy text of the printing and<br>typesetting industry.</div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="university-wrapper">
+        <div class="university-details">
+            <div class="">
+                <div class="university-info">
+                    <div class="university-profile">
+                        <div class="banner">
+                            <img src="../assets/image/univevrsity/banner.svg" alt="" title="" />
+                            <div class="logo"><img src="../assets/image/univevrsity/logo.svg" alt="" title="" /></div>
+                        </div>
+
+                        <div class="info">
+                            <h3>The University of Sydney</h3>
+                            <div class="location">
+                                <div class="name">
+                                    <img src="../assets/image/univevrsity/flag.png" alt="" title="" />
+                                    Australia
+                                </div>
+                                <div>
+                                    <img src="../assets/image/univevrsity/location.svg" alt="" title="" />
+                                    Camperdown NSW 2006, Australia
+                                </div>
+                            </div>
+
+                            <div class="offer">
+                                <img src="../assets/image/univevrsity/offer.svg" alt="" title="" />
+                                <span>Courses offered:</span> BBA, MBA, CSE, B.A. in English, M. Phil, Phd
+                            </div>
+                            <div class="social-share">
+                                <div class="share-title">
+                                    <img class="auth" src="../assets/image/blog/share.svg" alt="" title="">
+                                    <span>Share on</span>
+                                </div>
+                                <div class="social-link">
+                                    <a href="#">
+                                        <img class="banner-bg" src="../assets/image/icon/social-icon/color/facebook.svg" alt="" title="">
+                                    </a>
+                                    <a href="#">
+                                        <img class="banner-bg" src="../assets/image/icon/social-icon/color/twiter.svg" alt="" title="">
+                                    </a>
+                                    <a href="#">
+                                        <img class="banner-bg" src="../assets/image/icon/social-icon/color/linkdin.svg" alt="" title="">
+                                    </a>
+                                    <a href="#">
+                                        <img class="banner-bg" src="../assets/image/icon/social-icon/color/pinteret.svg" alt="" title="">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="desc">
+                        <h3>About University of Sydney</h3>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                        </p>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        </p>
+
+                        <div class="gallery">
+                            <div class="gallery-wrapepr">
+                                <div>
+                                    <img src="../assets/image/univevrsity/5.png" alt="" title="" />
+                                </div>
+                                <div class="small">
+                                    <div>
+                                        <img src="../assets/image/univevrsity/1.png" alt="" title="" />
+                                    </div>
+                                    <div>
+                                        <img src="../assets/image/univevrsity/2.png" alt="" title="" />
+                                    </div>
+                                    <div>
+                                        <img src="../assets/image/univevrsity/3.png" alt="" title="" />
+                                    </div>
+                                    <div>
+                                        <img src="../assets/image/univevrsity/4.png" alt="" title="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="university-cost">
+                <h3>Cost and Duration</h3>
+                <div>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                    </p>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                    <div class="cost-list">
+                        <div class="cost-btn">
+                            <div class="icon fee">
+                                <img src="../assets/image/univevrsity/icon/icon.svg" />
+                            </div>
+                            <div class="info">
+                                <h3>$50.00-$100.00 </h3>
+                                <span>Average Apllication Fee</span>
+                            </div>
+                        </div>
+                        <div class="cost-btn">
+                            <div class="icon graduate">
+                                <img src="../assets/image/univevrsity/icon/clock.svg" />
+                            </div>
+                            <div class="info">
+                                <h3>2 to 2.5 years</h3>
+                                <span>Graduate program</span>
+                            </div>
+                        </div>
+                        <div class="cost-btn">
+                            <div class="icon undergraduate">
+                                <img src="../assets/image/univevrsity/icon/clock.svg" />
+                            </div>
+                            <div class="info">
+                                <h3>3 to 4 years</h3>
+                                <span>Undergraduate program</span>
+                            </div>
+                        </div>
+                        <div class="cost-btn">
+                            <div class="icon cost-living">
+                                <img src="../assets/image/univevrsity/icon/home.svg" />
+                            </div>
+                            <div class="info">
+                                <h3>$5000.00/Year</h3>
+                                <span>Cost of living</span>
+                            </div>
+                        </div>
+                        <div class="cost-btn">
+                            <div class="icon tution-fee">
+                                <img src="../assets/image/univevrsity/icon/tution-fee.svg" />
+                            </div>
+                            <div class="info">
+                                <h3>$15000.00/Year</h3>
+                                <span>Tution fees</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <h3>Courses offered by University of Sydney</h3>
+                <div>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+
+                    <div>
+                        <a href="#">
+                            <i><img src="../assets/image/univevrsity/icon/clock.svg" /></i>
+                            Bachelor of Arts (Hons.)
+                        </a>
+                        <a href="#">
+                            <i><img src="../assets/image/univevrsity/icon/clock.svg" /></i>
+                            Bachelor of Arts (Hons.)
+                        </a>
+                        <a href="#">
+                            <i><img src="../assets/image/univevrsity/icon/clock.svg" /></i>
+                            Bachelor of Arts (Hons.)
+                        </a>
+                        <a href="#">
+                            <i><img src="../assets/image/univevrsity/icon/clock.svg" /></i>
+                            Bachelor of Arts (Hons.)
+                        </a>
+                        <a href="#">
+                            <i><img src="../assets/image/univevrsity/icon/clock.svg" /></i>
+                            Bachelor of Arts (Hons.)
+                        </a>
+                        <a href="#">
+                            <i><img src="../assets/image/univevrsity/icon/clock.svg" /></i>
+                            Bachelor of Arts (Hons.)
+                        </a>
+                        <a href="#">
+                            <i><img src="../assets/image/univevrsity/icon/clock.svg" /></i>
+                            Bachelor of Arts (Hons.)
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <h3>More Universities in Australia</h3>
+                <div>
+                    <div>
+                        <a href="#">
+                            <div class="">
+                                <img src="../assets/image/univevrsity/logo.svg" alt="" title="" />
+                            </div>
+                            <span> University of Sydney</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#">
+                            <div class="">
+                                <img src="../assets/image/univevrsity/logo.svg" alt="" title="" />
+                            </div>
+                            <span> University of Sydney</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#">
+                            <div class="">
+                                <img src="../assets/image/univevrsity/logo.svg" alt="" title="" />
+                            </div>
+                            <span> University of Sydney</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#">
+                            <div class="">
+                                <img src="../assets/image/univevrsity/logo.svg" alt="" title="" />
+                            </div>
+                            <span> University of Sydney</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Footer S-->
+    <?php include '../pages/footer.php'; ?>
+    <!-- Footer E-->
+
+
+
+
+    <script src="http://design.iasbd.co.uk/assets/js/bundle.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="http://design.iasbd.co.uk/assets/js/slick.js"></script>
+    <script type="text/javascript" src="http://design.iasbd.co.uk/assets/js/menu.js"></script>
+    <script type="text/javascript" src="http://design.iasbd.co.uk/assets/js/custom.js"></script>
+</body>
+
+</html>
