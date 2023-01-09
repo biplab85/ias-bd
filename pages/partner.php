b28a0bb17000469a07b272ac0862ed99179557a7
Biplab Paul <biplab@codersbucket.com>
2023-01-09 23:10:11 +0600

    css update

--- pages/partner.php
+++ pages/partner.php
@@ -3,43 +3,39 @@
         <h3 class="title">Partner With Us</h3>
         <span class="sub-title">Lorem Ipsum Dolor Sit Amet Adipiscing Elit</span>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 partner-list-wrapper">
-                <div class="partner-list"  style="background-image: url(../assets/image/partner/1.png);">
-                    <div class="details">
-                        <div class="short-details">
-                            <h4>Referral & Sub-Agents</h4>
-                            <p>
-                                Please speak to us about our referral partner and sub-agent programs.
-                            </p>
-                            <div class="contact">
-                                <span>
-                                    <img src="../assets/image/partner/call.svg" alt="" title="" /> +8801711875240
-                                </span>
-                                <span>
-                                    <img src="../assets/image/partner/mail.svg" alt="" title="" /> info@iasbd.co.uk
-                                </span>
-                            </div>
-                        </div>
+    <div class="partner-list-wrapper">
+        <div class="partner-list" style="background-image: url(../assets/image/partner/partner-1.svg);">
+            <div class="details">
+                <div class="short-details">
+                    <h4>Referral & Sub-Agents</h4>
+                    <p>
+                        Please speak to us about our referral partner and sub-agent programs.
+                    </p>
+                    <div class="contact">
+                        <span>
+                            <img src="../assets/image/partner/call.svg" alt="" title="" /> +8801711875240
+                        </span>
+                        <span>
+                            <img src="../assets/image/partner/mail.svg" alt="" title="" /> info@iasbd.co.uk
+                        </span>
                     </div>
                 </div>
-                <div class="partner-list"  style="background-image: url(../assets/image/partner/2.png);">
-                    <div class="details">
-                        <div class="short-details">
-                            <h4>Referral & Sub-Agents</h4>
-                            <p>
-                                Please speak to us about our referral partner and sub-agent programs.
-                            </p>
-                            <div class="contact">
-                                <span>
-                                    <img src="../assets/image/partner/call.svg" alt="" title="" /> +8801711875240
-                                </span>
-                                <span>
-                                    <img src="../assets/image/partner/mail.svg" alt="" title="" /> info@iasbd.co.uk
-                                </span>
-                            </div>
-                        </div>
+            </div>
+        </div>
+        <div class="partner-list second" style="background-image: url(../assets/image/partner/partner-2.svg);">
+            <div class="details">
+                <div class="short-details">
+                    <h4>Referral & Sub-Agents</h4>
+                    <p>
+                        Please speak to us about our referral partner and sub-agent programs.
+                    </p>
+                    <div class="contact">
+                        <span>
+                            <img src="../assets/image/partner/call.svg" alt="" title="" /> +8801711875240
+                        </span>
+                        <span>
+                            <img src="../assets/image/partner/mail.svg" alt="" title="" /> info@iasbd.co.uk
+                        </span>
                     </div>
                 </div>
             </div>
